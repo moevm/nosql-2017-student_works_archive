fff26a1070d027679d723fcb6e32a3c3c6236484
darya-lap <daryalaptsevich@gmail.com>
2017-11-22 21:55:21 +0300

    Spring is here!

--- server/src/main/java/Application.java
+++ server/src/main/java/Application.java
@@ -1,3 +1,4 @@
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,18 +11,19 @@ import java.util.Properties;
 public class Application  extends WebMvcConfigurerAdapter{
 
     public static void main(String[] args) throws UnknownHostException {
-        Properties prop = new Properties();
-        prop.setProperty("host", "localhost");
-        prop.setProperty("port", "27017");
-        prop.setProperty("dbname", "studArchive");
-        prop.setProperty("login", "admin1");
-        prop.setProperty("password", "111");
-        prop.setProperty("studentsTable", "students");
-        prop.setProperty("archiveTable", "archive");
-        WorkWithMongo mongo = new WorkWithMongo(prop);
-        Student student = new Student("4381","Ivan","Petrov","1321");
-        System.out.println(student);
-        mongo.addStudent(student);
-        System.out.println(mongo.getStudentByStudentID("4381"));
+        SpringApplication.run(Application.class, args);
+//        Properties prop = new Properties();
+//        prop.setProperty("host", "localhost");
+//        prop.setProperty("port", "27017");
+//        prop.setProperty("dbname", "studArchive");
+//        prop.setProperty("login", "admin1");
+//        prop.setProperty("password", "111");
+//        prop.setProperty("studentsTable", "students");
+//        prop.setProperty("archiveTable", "archive");
+//        WorkWithMongo mongo = new WorkWithMongo(prop);
+//        Student student = new Student("4381","Ivan","Petrov","1321");
+//        System.out.println(student);
+//        mongo.addStudent(student);
+//        System.out.println(mongo.getStudentByStudentID("4381"));
     }
 }
