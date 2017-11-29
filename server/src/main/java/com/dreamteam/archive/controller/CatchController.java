917894858e896bb105dd002b8a58f300c3797b33
gusch <guscha.vladislav1997@yandex.ru>
2017-11-29 22:47:14 +0300

    Запрос по одному параметру

--- server/src/main/java/com/dreamteam/archive/controller/CatchController.java
+++ server/src/main/java/com/dreamteam/archive/controller/CatchController.java
@@ -23,43 +23,43 @@ public class CatchController {
     }
 
     @GetMapping
-    @RequestMapping
+    @RequestMapping(value="/grade")
     public ResponseEntity<?> getQuery(@RequestParam("grade")String grade){
         return new ResponseEntity<List<Archive>>(catchService.getByGrade(grade), HttpStatus.OK);
     }
     @GetMapping
-    @RequestMapping
-    public ResponseEntity<?> getQueryTitle(@RequestParam("title")String title){
+    @RequestMapping(value="/title")
+    public ResponseEntity<?> getTitle(@RequestParam("title")String title){
         return new ResponseEntity<List<Archive>>(catchService.getByTitle(title), HttpStatus.OK);
     }
     @GetMapping
-    @RequestMapping
+    @RequestMapping(value="/date")
     public ResponseEntity<?> getQueryDate(@RequestParam("date")String date){
         return new ResponseEntity<List<Archive>>(catchService.getByDate(date), HttpStatus.OK);
     }
     @GetMapping
-    @RequestMapping
+    @RequestMapping(value="/studentId")
     public ResponseEntity<?> getQueryStudentId(@RequestParam("studentId")String studentId){
         return new ResponseEntity<List<Archive>>(catchService.getByStudentId(studentId), HttpStatus.OK);
     }
 
     @GetMapping
-    @RequestMapping
+    @RequestMapping(value="/subject")
     public ResponseEntity<?> getQuerySubject(@RequestParam("subject")String subject){
         return new ResponseEntity<List<Archive>>(catchService.getBySubject(subject), HttpStatus.OK);
     }
     @GetMapping
-    @RequestMapping
+    @RequestMapping(value="/teacherName")
     public ResponseEntity<?> getQueryTeacherName(@RequestParam("teacherName")String teacherName){
         return new ResponseEntity<List<Archive>>(catchService.getByTeacherName(teacherName), HttpStatus.OK);
     }
     @GetMapping
-    @RequestMapping
+    @RequestMapping(value="/studentName")
     public ResponseEntity<?> getQueryStudentName(@RequestParam("studentName")String studentName){
         return new ResponseEntity<List<Archive>>(catchService.getByStudentName(studentName), HttpStatus.OK);
     }
     @GetMapping
-    @RequestMapping
+    @RequestMapping(value="/group")
     public ResponseEntity<?> getQueryGroup(@RequestParam("group")String group){
         return new ResponseEntity<List<Archive>>(catchService.getByGroup(group), HttpStatus.OK);
     }
