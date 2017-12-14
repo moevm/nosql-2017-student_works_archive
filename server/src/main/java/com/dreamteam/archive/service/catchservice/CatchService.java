8a477a10f8c6c28a0f8cbdfd5633e2a7d6df9a21
gusch <guscha.vladislav1997@yandex.ru>
2017-12-14 04:09:43 +0300

    GridFs + Spring

--- server/src/main/java/com/dreamteam/archive/service/catchservice/CatchService.java
+++ server/src/main/java/com/dreamteam/archive/service/catchservice/CatchService.java
@@ -3,15 +3,8 @@ package com.dreamteam.archive.service.catchservice;
 import com.dreamteam.archive.model.Archive;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CatchService {
-    public List<Archive> getByGrade(String grade);
-    public List<Archive> getByTitle(String title);
-    public List<Archive> getBySubject(String subject);
-    public List<Archive> getByTeacherName(String teacherName);
-    public List<Archive> getByStudentId(String studentId);
-    public List<Archive> getByStudentName(String studentName);
-    public List<Archive> getByGroup(String group);
-    public List<Archive> getByDate(String date);
-
+    List<Archive> getFind(Map<String,String> json);
 }
